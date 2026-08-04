330c5be3ff9684a101c03f3b0b4fc5f252eb2ecd
burgerizzrmshwy-boop <burgerizzrmshwy@gmail.com>
2026-08-04 13:22:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Burgerizzr Mashwi Website Loaded");
 
+    const button = document.querySelector(".btn");
+
+    if (button) {
+        button.addEventListener("click", function () {
+            alert("Thank you for choosing Burgerizzr Mashwi! 🍔");
+        });
+    }
+});
